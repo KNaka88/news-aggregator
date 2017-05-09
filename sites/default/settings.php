39250d5e1055a97a8c03d65206b0b9fe5790f3dc
Koji Nakagawa and Sean Peterson <sean@>
2017-05-09 14:06:15 -0700

    add db-backup

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => 'news-aggregator',
+      'username' => 'admin',
+      'password' => 'admin',
+      'host' => 'localhost',
+      'port' => '8889',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'PKSdBIk4WKSi1skRks4Z33Y4VMRJoP1oy2nJaRdZpnk';
 
 /**
  * Base URL (optional).
